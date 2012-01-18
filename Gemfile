d35e535f5bb2aab2102c4bf21524d3eec56eb176
Mario Uher <uher.mario@gmail.com>
2012-01-18 20:18:09 +0100

    First commit.

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'thin'
+
+# Assets
+gem 'bourbon'
+gem 'coffee-script'
+gem 'execjs'
+gem 'sass'
+gem 'sprockets'
+gem 'therubyracer'
---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require
+
+# Set standard views path
+set :views, settings.root + '/app/views'
+
+# Define root path
+ROOT_DIR = File.expand_path(File.dirname(__FILE__))
+def root_path(*args)
+  File.join(ROOT_DIR, *args)
+end
+
+# Load app files
+Dir[root_path("app/*.rb")].each do |file|
+  require file
+end
+
+# Load config files
+Dir[root_path("config/*.rb")].each do |file|
+  require file
+end
